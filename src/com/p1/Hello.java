94764fd8eab37461428106b06a0cafd529598f89
Aakash Sir <ay378581@gmail.com>
2024-06-21 17:16:07 +0530

    1-L Add

--- src/com/p1/Hello.java
+++ src/com/p1/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("hello by aman");
 		System.out.println("Hello by Aman 2.0");
+		System.out.println("Aakash");
 	}
 
 }
